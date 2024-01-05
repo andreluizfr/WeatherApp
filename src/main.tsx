0a2ecbf8d29417b10fbb7ff324d34f71c1a6f992
André <alfr2@cin.ufpe.br>
2024-01-05 20:58:40 -0300

    refactor: renomeando App.tsx para router.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './router.tsx';
 import '@assets/sass/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<Router />
 	</React.StrictMode>,
 )
