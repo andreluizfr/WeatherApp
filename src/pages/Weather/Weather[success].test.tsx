523c6e939d92834ed80a4b964b6669c9cfb51eb9
André <alfr2@cin.ufpe.br>
2024-01-05 19:46:14 -0300

    fix: renomeando arquivos de service que estavam com problema de case

--- src/pages/Weather/Weather[success].test.tsx
+++ src/pages/Weather/Weather[success].test.tsx
@@ -11,8 +11,8 @@ import WeatherPage from '.';
 
 import HourlyWeatherResponse from '@entities/HourlyWeatherResponse';
 
-import getCurrentWeather from '@services/geturrentWeather';
-import getHourlyWeather from '@services/getourlyWeather';
+import getCurrentWeather from '@services/getCurrentWeather';
+import getHourlyWeather from '@services/getHourlyWeather';
 
 import searchNextDayTemperature from '@utils/searchNextDayTemperature';
 import getSunrise from '@utils/getSunrise';
